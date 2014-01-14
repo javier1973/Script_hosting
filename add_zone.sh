ff8780c6b32fc5a0fa80bf771218ad58808fb8cb
javier1973 <fjjaviergv@yahoo.es>
2014-01-14 10:27:28 +0100

    inicio de script para crear zona

---
+++ add_zone.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'zone "'$1'" {' >> /var/cache/bind/$2
+echo '        type master;' >> /var/cache/bind/$2
+echo '      file "'$2'";' >> /var/cache/bind/$2
+echho '};' >> /var/cache/bind/$2
+
+zone "0.0.10.in-addr.arpa"{
+        type master;
+        file "inversa_javihost";
+
