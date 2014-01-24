35025b003814dcf865a25a03865dc756e770a262
javier1973 <fjjaviergv@yahoo.es>
2014-01-24 10:03:44 +0100

    terminado script para crear zonas, falta definirlas

--- add_zone.sh
+++ add_zone.sh
@@ -5,17 +5,8 @@ echo '        type master;' >> /var/cache/bind/$2
 echo '      file "'$2'";' >> /var/cache/bind/$2
 echo '};' >> /var/cache/bind/$2
 
-echo 'zone "0.0.10.in-addr.arpa"{'
-        type master;
-        file "inversa_javihost";
-	};
-
-echo 'zone "'$1'" {' >> $2
-echo '        type master;' >> $2
-echo '      file "'$2'";' >> $2
-echo '};' >> $2
 
 echo 'zone "'$3'.in-addr.arpa"{'>> /var/cache/bind/'inv_'$2
 echo '        type master;'>> /var/cache/bind/'inv_'$2
 echo '        file "inv_"$2;'>> /var/cache/bind/'inv_'$2
-echo '	};' >> $2
+echo '	};' >> /var/cache/bind/$2
