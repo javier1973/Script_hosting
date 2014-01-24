8a29a1cf6c30d2507408a594fc028cec9395b3aa
javier1973 <fjjaviergv@yahoo.es>
2014-01-24 10:02:11 +0100

    terminado script para crear zonas, falta definirlas

--- add_zone.sh
+++ add_zone.sh
@@ -15,7 +15,7 @@ echo '        type master;' >> $2
 echo '      file "'$2'";' >> $2
 echo '};' >> $2
 
-echo 'zone "'$3'.in-addr.arpa"{'>> 'inv_'$2
-echo '        type master;'>> 'inv_'$2
-echo '        file "inv_"$2;'>> 'inv_'$2
+echo 'zone "'$3'.in-addr.arpa"{'>> /var/cache/bind/'inv_'$2
+echo '        type master;'>> /var/cache/bind/'inv_'$2
+echo '        file "inv_"$2;'>> /var/cache/bind/'inv_'$2
 echo '	};' >> $2
