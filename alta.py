f21a9c8b002422dd965d6096bb1fa23c12bcaa99
root <root@debian>
2014-02-12 00:23:16 +0100

    corregido errores de sintaxis

--- alta.py
+++ alta.py
@@ -12,7 +12,7 @@ def gen_passw():
 	passw = passw.join([choice(valores) for i in range(longitud)])
 	return passw
 
-def crea_bd():	
+def crea_bd(nombre,dominio):	
 	## creamos Base de datos para el cliente ##
 	pass_mysql=gen_passw()
 	print "A continuación se va a generar la contraseña para mysql"
@@ -20,51 +20,58 @@ def crea_bd():
 	print "la contraseña para %s es: %s" % (nombre,pass_mysql)
 	#creamos una bas de datos para el usuario
 	crearbase=" create database %s" % (nombre)
-    cursor.execute(crearbase)
+	cursor.execute(crearbase)
     #creamos el usuario y le damos todos los permisos sobre la base de datos creada antes
-    permisosql= "grant all privileges on %s.* to"% (nombre)+ " %s@localhost"% (nombre)+ " identified by "+"'%s'" % (pass_mysql)
-    cursor.execute(permisosql)
+	permisosql= "grant all privileges on %s.* to"% (nombre)+ " %s@localhost"% (nombre)+ " identified by "+"'%s'" % (pass_mysql)
+	cursor.execute(permisosql)
 	#guardamos y salimos
-    basereload = "FLUSH PRIVILEGES;"
-    cursor.execute(basereload)
-    base.commit()
+	basereload = "FLUSH PRIVILEGES;"
+	cursor.execute(basereload)
+	base.commit()
 
 
-def crea_carpetas():
+def crea_carpetas(nombre,dominio):
 	## CARPETAS PERSONALES y VIRTUAL HOST##
-    os.system("mkdir /srv/www/%s" % opcion1)
-    os.system("echo Pagina del dominio %s en construccion > /srv/www/%s/index.html" %(dominio,nombre))
+    os.system("mkdir /usr/www/%s" % nombre)
+    os.system("echo Pagina del dominio %s en construccion > /usr/www/%s/index.html" %(dominio,nombre))
     #creamos vitual host y guardamos en /etc/apache/sites-avaible
     vhost=open("virtualhosts","r")
     lista=vhost.read()
-	vhost.close()
+    vhost.close()
     lista=lista.replace("@dominio@","%s" % dominio)
     lista=lista.replace("@nombre@","%s" % nombre)
     #abrimos el modo escritura y crea un nuevo fichero con lo introducido anteriormente
     s_avaible=open("/etc/apache2/sites-available/%s" % dominio,"w")
     s_avaible.write(lista)
     s_avaible.close()
+    os.system("a2ensite %s >/dev/null" % dominio)
+    os.system("service apache2 restart >/dev/null ")
 
-def crea_proftpd():    
+def crea_proftpd(nombre,dominio):    
 	# PROFTPD ##
     #con pwgen obtenemos una contrasena aleatoria de 12 letras y la guardamos en un fichero
-    pass=gen_passw()
+    pas=gen_passw()
     consulta = "select max(uid) from proftpd.usuarios"
     cursor.execute(consulta)
-	uid = cursor.fetchone()
-	if uid == None:
-		uid = 4000
-	else:
-		resultado = resultado + 1
-	print "La contraseña de Proftpd para el usuario %s es : %s" % (nombre,pass)
+    uids = cursor.fetchone()   
+    uid=uids[0] + 1   
+    if uids != None:
+		uid = 1 + uids[0]
+	
     #Insetamos al nuevo usuario en la tabla usuarios con la clave encriptada
-    addusuario = "insert into usuarios values ('%s', PASSWORD('%s'), '%s', 6000, '/srv/www/%s','/bin/false',1,'%s');" % (nombre,pass,uid,nombre,dominio)
-    cursor.execute(anadirusua)
+    print "La contraseña de Proftpd para el usuario %s es : %s" % (nombre,pas)
+    addusuario = "insert into usuarios values ('%s', PASSWORD('%s'), '%s', '%s', '/usr/www/%s','/bin/false',1,'%s');" % (nombre,pas,uid,uid,nombre,dominio)
+    addgrupo = "insert into grupos values ('%s',%s)" % (nombre,uid)
+    cursor.execute(addusuario)
+    cursor.execute(addgrupo)
     basereload = "FLUSH PRIVILEGES;"
     cursor.execute(basereload)
     base.commit()
+    os.system("chmod -R 754 /usr/www/%s" % (uid, uid, nombre))
+    os.system("chown -R %s:%s /usr/www/%s" % (uid, uid, nombre))
+    
 
-def crea_dns():        
+def crea_dns(nombre,dominio):        
     ## CREAR DNS ##
     # llemos plantilla nombre zona
     nombre_zona=open("named.conf.local","r")
@@ -73,11 +80,10 @@ def crea_dns():
 	#leemos plantilla zona directa
     zona_directa=open("db.dominio","r")
     lista_db=zona_directa.read()
-	zona_directa.close()
+    zona_directa.close()
         
     #reemplazamos en el fichero el dominio introducido
-    lista_nombre=lista_nombre.replace("@dominio@","%s" % dominio)
-       
+    lista_nombre=lista_nombre.replace("@dominio@","%s" % dominio)  
     #abrimos el fichero en modo añadir y agregamos la nueva zona
     nombre_zona=open("/etc/bind/named.conf.local","a")
     nombre_zona.write(lista_nombre)
@@ -86,9 +92,9 @@ def crea_dns():
     #reemplazamos en el fichero el dominio introducido
     lista_db=lista_db.replace("@dominio@","%s" % dominio)
     #abrimos el fichero en modo escritura y lo guardamos con lo modificado antes
-    zona_directa=open("/var/cache/bind/db.%s" % (dominio,"w")
-    zona_directa.write(lista_db)
-    zona_directa.close()
+    z_directa=open("/var/cache/bind/db.%s" % (dominio),"w")
+    z_directa.write(lista_db)
+    z_directa.close()
     os.system("service bind9 restart >/dev/null")
 
 
@@ -103,7 +109,7 @@ base = MySQLdb.connect(host="localhost", user="root", passwd="javi", db="proftpd
 cursor = base.cursor()
 
 #realizaremos consulta para comprobar que no existe usuario 
-consulta = "select usuario from proftpd.usuarios where usuario='%s';" % (nombre,dominio)
+consulta = "select usuario from usuarios where usuario='%s';" % (nombre)
 cursor.execute(consulta)
 devolucion = cursor.fetchone()
 if devolucion != None:
@@ -117,8 +123,8 @@ else:
 		print "Introduzca otro dominio, el dominio %s ya existe" % (dominio)
 	else:
 		crea_bd(nombre,dominio)
-		crea_carpetas():
-		crea_proftpd()
+		crea_carpetas(nombre,dominio)
+		crea_proftpd(nombre,dominio)
 		crea_dns(nombre,dominio)        
      
         
